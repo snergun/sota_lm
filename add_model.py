a8d2ae7738a566f1be35b2fbb69d1c34dda2627d
Esen <eergun@ucsd.edu>
2025-09-06 02:46:09 +0000

    new results

--- add_model.py
+++ add_model.py
@@ -1,21 +1,26 @@
 import torch 
 import numpy as np
 import os
-model_name = "KNN"
-results_path = "/home/jovyan/pos_lm_v2/data/knn-wiki-cache"
+model_name = "pw_knn_lm"
+results_path = "/home/jovyan/pos_lm_v2/notebooks/results/pw_knn_lm_prob"
 data_lens = {"validation": 217646, "test": 245569}
 splits = ["validation", "test"]
+# probs = {
+#     split : np.exp(
+#         np.memmap(
+#                 os.path.join(results_path,f"{split}_prob.npy"),
+#                 dtype='float32',
+#                 mode='r',
+#                 shape=(data_lens[split],)
+#                            )
+#         ) for split in splits
+#     }
 probs = {
     split : np.exp(
-        np.memmap(
-                os.path.join(results_path,f"{split}_prob.npy"),
-                dtype='float32',
-                mode='r',
-                shape=(data_lens[split],)
+        np.array(np.load(os.path.join(results_path,f"{split}_prob.npy"),  mmap_mode='r'))
                            )
-        ) for split in splits
+        for split in splits
     }
-
 #Write probs to file, each prob in new line:
 with open(f"WikiText-103/valid/{model_name}.txt", "w") as f:
     for prob in probs["validation"]:
--- main.sh
+++ main.sh
@@ -2,5 +2,5 @@
 #     --models AdaptiveInputs DELIGHT GCN KNN MEGA S4 ShortFormer \
 #     >> results.txt
 python main.py \
-    --models AdaptiveInputs KNNLM MEGA \
+    --models pw_knn_lm KNNLM MEGA S4 ShortFormer \
     >> results.txt
